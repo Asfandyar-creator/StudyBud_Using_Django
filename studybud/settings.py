f026bc21e7dfd1580a5c4f0bc2e9da9b06ed4183
Asfandyar <yar69416@gmail.com>
2024-05-04 19:52:06 +0500

    chore: Update STATIC_ROOT in settings.py to include multiple static files

--- studybud/settings.py
+++ studybud/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -136,7 +136,7 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 
 
 
-STATIC_ROOT = BASE_DIR / 'static/style.css'
+STATIC_ROOT = os.path.join(BASE_DIR, ['style.css', 'main.css', 'main.js'])
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
