1514125002c229882295c0883c4050c80ec3816f
Asfandyar <yar69416@gmail.com>
2024-05-04 18:30:13 +0500

    chore: Update STATIC_ROOT in settings.py and add alt text to logo image

--- studybud/settings.py
+++ studybud/settings.py
@@ -136,7 +136,7 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 
 
 
-# STATIC_ROOT = 
+STATIC_ROOT = BASE_DIR / 'static/styles.css'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
